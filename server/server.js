0dff431d6d909b696c81034bf98243e5625521f1
Koh Chang Hui <changhui92@Kohs-MacBook-Pro-2.local>
2017-08-09 21:04:54 +0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -111,6 +111,20 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// POST /users
+app.post('/Users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`started up at port ${port}`);
 });
